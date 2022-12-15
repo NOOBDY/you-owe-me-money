c7c9d0d081fd68d6e40c266706fe6194924e018e
Wen-Hsuan Chou <110590015@ntut.org.tw>
2022-12-15 13:15:36 +0800

    append record

--- main.py
+++ main.py
@@ -2,6 +2,52 @@ from os import environ
 
 import discord
 
+
+class UserData:
+    _myself = ""  # username
+    _debtors = dict()  # for all the debtor and the total money of each
+    _records = list()
+
+
+class Record:
+    _myself = ""  # username
+    _debtor = ""  # debtor who borrow money from you
+    _date = ""  # the date that event happened
+    _amount = 0  # how much you lent him or her
+    _info = ""  # the info of the item
+
+    def __init__(self, debtor, date, amount, info):
+        self._debtor = debtor
+        self._date = date
+        self._amount = amount
+        self._info = info
+
+
+def setUpUser():  # set a new record for a user
+    print("setup here")
+
+
+def newRecord(command, myself):  # append a record for a certain user
+    temp = command.split()
+    myself = "<@"+str(myself)+">"
+    new = Record(temp[1], temp[2], temp[3], temp[4])
+    send = new._debtor+" borrowed "+str(new._amount)+" from " + myself + " because of  " + \
+        new._info+" on "+new._date
+    return send
+
+
+def modify():  # modify the record
+    print("modify here")
+
+
+def remove():  # remove the record
+    print("remove here")
+
+
+def check():  # print the debt data
+    print("check here")
+
+
 if __name__ == "__main__":
     intents = discord.Intents.default()
     intents.message_content = True
@@ -14,10 +60,30 @@ if __name__ == "__main__":
 
     @client.event
     async def on_message(message):
-        if message.author == client.user:
+        command = ""
+        if (message.author == client.user):
             return
 
-        if message.content.startswith("$hello"):
-            await message.channel.send("Hello!")
+        print(message)
+        print("======================\n")
+        print(message.content)
 
-    client.run(environ["DISCORD_BOT_TOKEN"])
+        if (message.content[0:6] == "record"):
+            command = message.content
+            await message.channel.send(newRecord(command, message.author.id))
+
+        if (message.content[:4] == ("new")):
+            await message.channel.send("on progressing function new")
+
+        if (message.content[:6] == ("modify")):
+            await message.channel.send("on progressing function modify")
+
+        if (message.content[:6] == ("remove")):
+            await message.channel.send("on progressing function remove")
+
+        if (message.content[:5] == ("check")):
+            await message.channel.send("on progressing function check")
+
+    # client.run(environ["DISCORD_BOT_TOKEN"]
+    client.run(
+        "MTA1MTAyMTc5NjM0ODAwNjQ1MQ.G2pzRn.caXY2YiXIoXEuBCUxfyaVXquPsB6WrPg9Jrcz4")
