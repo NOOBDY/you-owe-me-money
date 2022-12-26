9dc2b76a5a128a95d55708386f0357af08ac6e31
Chinococo <garyliuyahoocom384@gmail.com>
2022-12-27 01:22:06 +0800

    add_record and IsCheckid

--- db/utils.py
+++ db/utils.py
@@ -1,12 +1,17 @@
 """
 utils module
 """
-
+from os import environ
 from .record import Record
-
+url: str = "https://dwosibtkxkverqqwtsnp.supabase.co"
+key: str = environ["SUPABASE_API_KEY"]
+supabase: Client = create_client(url, key)
 
 def add_record(record: Record) -> None:
-    ...
+    try:
+        supabase.table("Record").insert(record.RecordToDict()).execute()
+    except:
+        pass
 
 
 def delete_record(record_id: int) -> None:
@@ -23,3 +28,6 @@ def creditor_records(creditor_id: int) -> list[Record]:
 
 def debtor_records(debtor_id: int) -> list[Record]:
     ...
+
+def IsCheckid(id)->bool:
+    return len(supabase.table("Record").select("*").filter("record_id", "eq", key).execute().data)!=0
