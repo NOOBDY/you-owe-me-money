3ade8cb1ea45d75f32ed17d3ee39d4de12360db2
Chinococo <garyliuyahoocom384@gmail.com>
2022-12-27 01:28:19 +0800

    Add Database Method

--- db/utils.py
+++ db/utils.py
@@ -12,22 +12,34 @@ def add_record(record: Record) -> None:
         supabase.table("Record").insert(record.RecordToDict()).execute()
     except:
         pass
-
+    return None
 
 def delete_record(record_id: int) -> None:
-    ...
-
+    try:
+        supabase.table("Record").delete().filter("record_id", "eq", record_id).execute()
+    except:
+        pass
+    return None    
 
 def find_record(record_id: int) -> Record:
-    ...
-
+    try:
+        return supabase.table("Record").select("*").filter("record_id", "eq", record_id).execute()
+    except:
+        pass
+    return None    
 
 def creditor_records(creditor_id: int) -> list[Record]:
-    ...
-
+    try:
+        return supabase.table("Record").select("*").filter("creditor_id", "eq",creditor_id).execute()
+    except:
+        pass
+    return None
 
 def debtor_records(debtor_id: int) -> list[Record]:
-    ...
+    try:
+        return supabase.table("Record").select("*").filter("debtor_id", "eq", debtor_id).execute()
+    except:
+        pass
+    return None    
 
-def IsCheckid(id)->bool:
-    return len(supabase.table("Record").select("*").filter("record_id", "eq", key).execute().data)!=0
+
