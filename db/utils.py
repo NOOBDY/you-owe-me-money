aece498ea1585a7fd0d156339a345cbb6b4f381e
110590064 <t110590064@ntut.edu.tw>
2022-12-28 12:44:51 +0800

    utils.py modfiy and add DictToRecord

--- db/utils.py
+++ db/utils.py
@@ -21,29 +21,39 @@ def delete_record(record_id: int) -> None:
         supabase.table("Record").delete().filter("record_id", "eq", record_id).execute()
     except:
         pass
-    return None    
+    return None
 
 def find_record(record_id: int) -> Record:
     try:
         res = supabase.table("Record").select("*").filter("record_id", "eq", record_id).execute().data
         if len(res)==0:
             return None
-        return res[0]
+        return DictToReocrd(res[0])
     except:
         pass
-    return None    
+    return None
 
 def creditor_records(creditor_id: int) -> list[Record]:
     try:
-        return supabase.table("Record").select("*").filter("creditor_id", "eq",creditor_id).execute().data
+        return [DictToReocrd(temp)for temp in supabase.table("Record").select("*").filter("creditor_id", "eq",creditor_id).execute().data]
     except:
         pass
     return None
 
 def debtor_records(debtor_id: int) -> list[Record]:
     try:
-        return supabase.table("Record").select("*").filter("debtor_id", "eq", debtor_id).execute().data
+        return [DictToReocrd(temp)for temp in supabase.table("Record").select("*").filter("debtor_id", "eq", debtor_id).execute().data]
     except:
         pass
     return None
 
+
+def DictToReocrd(data):  # 轉換成record格式
+    creditor_id = data["creditor_id"]
+    debtor_id = data["debtor_id"]
+    amount = data["amount"]
+    title = data["title"]
+    detail = data["detail"]
+    due_date = data["due_date"]
+    record_id = data["record_id"]
+    return Record(creditor_id, debtor_id,amount, title, detail,due_date)
